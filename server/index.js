54bdb4ac6157510a6660671583b1252b44e1dbe1
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-09 08:15:54 +0800

    express app generated

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// Generating Express apps
+const express = require("express"); // imports Express package
+const app = express(); // initiates a new Express app
+
+// Initiating Express route handlers
+  // 1) app - new route handlers to be registered to this app
+  // 2) get - new route handlers will be set up to handle with the "get" request
+  // 3) / - watch for incoming requests that are attempting to access a specific route (e.g. localhost:5000)
+  // 4) req - represents the incoming request
+  // 5) res - represents the outgoing response
+  // 6) res.send - close the request and send back a JSON response
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+// Instructs Express to tell Node to listen to incoming traffic on Port 5000
+app.listen(5000);
