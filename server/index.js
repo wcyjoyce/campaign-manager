847e2363601291799b3b5aee2d2b1b8210405fee
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-11 12:44:43 +0800

    google oauth set up with passportJS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,35 @@
 // Generating Express apps
 const express = require("express"); // imports Express package
+const passport = require ("passport"); // imports Passport JS package
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys.js");
+
 const app = express(); // initiates a new Express app
+
+// User OAuth handled by Passport JS
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback" // route that user is redirected to when he/she grants authorisation
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log("access token:", accessToken);
+    console.log("refresh token:", refreshToken);
+    console.log("profile:", profile);
+  })
+);
+
+
+// Once user enters through specified route (i.e. "/auth/google"), Passport JS is to authenticate user via a strategy called "Google"
+// Scope specifies the types of access that is obtained from user's profile
+app.get("/auth/google", passport.authenticate(
+  "google", {
+    scope: ["profile", "email"]
+  })
+);
+
+// When user gets redirected to "/auth/google/callback", route handler takes user's request and conducts code exchange
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // Initiating Express route handlers
   // 1) app - new route handlers to be registered to this app
