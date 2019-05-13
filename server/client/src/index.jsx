de2501f20696c660ed90e239dea2f399070c7b0c
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-13 15:27:06 +0800

    reducers set up

--- server/client/src/index.jsx
+++ server/client/src/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 
 import "./stylesheets/application.scss";
 import App from "./components/app.jsx";
 
+import authReducer from "./reducers/auth_reducer.js";
+
+const reducers = combineReducers({
+  auth: authReducer
+});
+
+const store = createStore(() => reducers, {}, applyMiddleware());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById("root")
 );
