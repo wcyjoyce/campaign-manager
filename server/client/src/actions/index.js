a041fef6d344a42ed6a542d209079e347bb0ec44
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-15 00:53:11 +0800

    refactored FETCH_USER action with async/await syntax

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,6 +4,12 @@ import { FETCH_USER } from "./types.js";
 export const fetchUser = () => {
   return function(dispatch) {
     axios.get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+      .then(response => dispatch({ type: FETCH_USER, payload: response.data  }));
   };
 };
+
+// Refactoring with Async/Await
+// export const fetchUser = () => async dispatch => {
+//   const response = await axios.get("/api/current_user");
+//   dispatch({ type: FETCH_USER, payload: response.data });
+// };
