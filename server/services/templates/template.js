e41e800a2e5200c7bb9a1d6eb7c291f80555bf52
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-23 07:06:15 +0800

    email template and CSS updated

--- server/services/templates/template.js
+++ server/services/templates/template.js
@@ -1,3 +1,16 @@
 module.exports = survey => {
-  return "<div>" + survey.body + "</div>";
+  return `
+    <html>
+      <body>
+        <div className="template">
+          <h1>I'd like your input!</h1>
+          <p>${survey.body}</p>
+          <div className="feedback">
+            <a href="http://localhost:3000/">Yes</a>
+            <a href="http://localhost:3000/">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
 };
