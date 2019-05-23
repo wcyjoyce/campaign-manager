63c7d43c879b035e87478ecaffa733676741516c
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-24 06:48:20 +0800

    refactoring to enable multiple different custom field compoennts

---
+++ server/client/src/components/surveys/field.jsx
@@ -0,0 +1,12 @@
+// import React, { Component } from "react";
+import React from "react";
+
+const InputField = ({ input }) => {
+  return (
+    <div>
+      <input {...input} />
+    </div>
+  );
+};
+
+export default InputField;
--- server/client/src/components/surveys/form.jsx
+++ server/client/src/components/surveys/form.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
+import InputField from "./field.jsx";
+
 class Form extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={InputField} />
+      </div>
+    );
+  };
+
   render() {
     return (
       <div>
       <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-        <Field type="text" name="title" component="input" placeholder="Title" />
+        {this.renderFields()}
         <button type="submit">Submit</button>
       </form>
       </div>
