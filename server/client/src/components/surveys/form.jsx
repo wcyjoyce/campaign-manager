bb967e919bc7b7160f5fdc55c08ab3aed9a5fdbd
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-24 07:45:20 +0800

    simple CSS added for form, cancel button added

--- server/client/src/components/surveys/form.jsx
+++ server/client/src/components/surveys/form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 import _ from "lodash";
 
 import InputField from "./field.jsx";
@@ -26,16 +27,19 @@ class Form extends Component {
   // refactoring input fields to keep it DRY
   renderFields() {
     return _.map(FIELDS, ({ label, name }) => {
-      return <Field component={InputField} type="text" label={label} name={name} key={name} />
+      return <Field className="field" component={InputField} key={name} type="text" label={label} name={name} placeholder={name} />
     });
   };
 
   render() {
     return (
-      <div>
+      <div className="form">
       <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-        {this.renderFields()}
-        <button type="submit">Submit</button>
+        <div className="fields">{this.renderFields()}</div>
+        <div className="field-actions">
+          <Link to="/surveys" className="btn btn-info">Cancel</Link>
+          <button type="submit" className="btn btn-info">Submit</button>
+        </div>
       </form>
       </div>
     );
