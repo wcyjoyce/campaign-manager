ac012398404254174dcae7cb162ec1944a8b96c4
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-25 18:40:54 +0800

    review page contents done

--- server/client/src/components/surveys/review.jsx
+++ server/client/src/components/surveys/review.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
 
-const Review = ({ onBack }) => {
-  return (
-    <div className="review">
-      <h2>Please Review</h2>
-      <div className="field-actions">
-        <button className="btn btn-warning" onClick={onBack}>Back</button>
-        <button className="btn btn-success">Submit</button>
+import formFields from "./surveyFields.js";
+
+class Review extends Component {
+  reviewInput() {
+    return _.map(formFields, field => {
+      return (
+        <table key={field.name}>
+          <tr>
+            <td>{field.label}</td>
+            <td>{this.props.formValues[field.name]}</td>
+          </tr>
+        </table>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div className="review">
+        <h2>Please Review</h2>
+        <div className="form-content">
+          {this.reviewInput()}
+        </div>
+        <div className="field-actions">
+          <button className="btn btn-warning" onClick={this.props.onBack}>Back</button>
+          <button className="btn btn-success">Submit</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
-export default Review;
+function mapStateToProps(state) {
+  return { formValues: state.form.surveyForm.values };
+};
+
+export default connect(mapStateToProps)(Review);
